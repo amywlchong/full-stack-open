bde5a66f09b0e5caf76912ea3a88556e6ea9ca7f
Amy Chong <amywlchong1@gmail.com>
2023-06-26 10:45:00 +0800

    added testing router

--- part4-testing-servers-and-user-administration/blog-list-backend/app.js
+++ part4-testing-servers-and-user-administration/blog-list-backend/app.js
@@ -28,9 +28,14 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(tokenExtractor)
 
-app.use('/api/blogs', userExtractor, blogsRouter)
-app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', userExtractor, blogsRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
